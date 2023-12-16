9cc7c0350b8a47c53336c98c038c86eaa5752adc
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-12-16 23:58:57 +0600

    Initialize insert function for doubly linked list

--- LinkedList/doublyLinkedList.cpp
+++ LinkedList/doublyLinkedList.cpp
@@ -6,7 +6,7 @@ struct Node
     int data;
     Node* next;
     Node* prev;
-
+    Node(int value) : data(value), next(nullptr), prev(nullptr) {}
 };
 
 class DoublyLinkedList {
@@ -24,10 +24,45 @@ class DoublyLinkedList {
         bool clear();
         bool search(int);
         void sort();
+        void reverse();
         void printList();
 };
+DoublyLinkedList::DoublyLinkedList() {
+    head = NULL;
+}
+bool DoublyLinkedList::insertAtEnd(int data) {
+    if(head == NULL) {
+        head = new Node(data);
+        return true;
+    }
+    Node* newNode = new Node(data);
+    Node* currNode = head;
+    while(currNode->next != NULL) {
+        currNode = currNode->next;
+    }
+    currNode->next = newNode;
+    newNode->prev = currNode;
+    return true;
+}
+void DoublyLinkedList::printList() {
+    Node* currNode = head;
+    while (currNode != NULL)
+    {
+        cout<< currNode->data<< " ";
+        currNode = currNode->next;
+    }
+    cout<< endl;
+}
 
 int main() {
+
+    DoublyLinkedList list;
+    list.insertAtEnd(10);
+    list.insertAtEnd(20);
+    list.insertAtEnd(30);
+
+    list.printList();
+
 
     return 0;
 }
